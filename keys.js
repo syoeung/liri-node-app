e8cc64776d33df8e6344cadf0a89d74478408c9c
Steven <yoeung@outlook.com>
2018-07-21 23:42:54 -0400

    changes

---
+++ keys.js
@@ -0,0 +1,15 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'm4Ot0Ddav7OPRu2TOQBZI5O7H',
+  consumer_secret: 'rBymtHEc0hP12G2gI9op34XR7LPpZ0WYucNpft5wGjRtbEE2z3',
+  access_token_key: '1018569715302715392-0o9MQ8HgUiqw5h0ZBA2RtG4bBkyWxw',
+  access_token_secret: 'W4Qq2MTWsn6d7rLaaxM8zru9RGTifrNredjeBVZFFdtFl',
+}
+
+exports.tmdbKey = '8d5f41f6535c312ee8ea248cecd733f7';
+
+exports.spotifyKeys = {
+	id: '660ee86f81254e78809763fe44eaa343',
+	secret: 'e2a1a2c9ea2347588f69d23d8cb85b72',
+}
